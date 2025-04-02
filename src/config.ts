75383b9383790d728eb92dc1b8f9ea6959a084cc
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2025-04-02 23:14:43 +0600

    try fix

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,1 @@
-// Get the current host and use it to construct API URL
-const getCurrentHost = () => {
-  const { hostname, protocol } = window.location;
-  return `${protocol}//${hostname}`;
-};
-
-// Use window location to determine API URL
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 
-  // If env variable not set, use the same host but with port 8080
-  `${getCurrentHost()}:8080`; 
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8080'; 
